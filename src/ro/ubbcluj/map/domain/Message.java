d28ad33b2fa06cb0cccbc04473cfe37be4aabb01
Mihai <mihaibodnaruc70@gmail.com>
2021-12-02 19:37:24 +0200

    message class added

--- src/ro/ubbcluj/map/domain/Message.java
+++ src/ro/ubbcluj/map/domain/Message.java
@@ -1,2 +1,71 @@
-package ro.ubbcluj.map.domain;public class Message {
+package ro.ubbcluj.map.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+public class Message extends Entity<Long> {
+    private final User from;
+    private List<User> to;
+    private String message;
+    private LocalDateTime date;
+
+    public Message(Long id, User from, List<User> to, String message, LocalDateTime date){
+        super(id);
+        this.from = from;
+        this.to = to;
+        this.message = message;
+        this.date = date;
+    }
+
+    public User getFrom() {
+        return from;
+    }
+
+    public List<User> getTo() {
+        return to;
+    }
+
+    public void setTo(List<User> to) {
+        this.to = to;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message1 = (Message) o;
+        return Objects.equals(from, message1.from) && Objects.equals(to, message1.to) && Objects.equals(message, message1.message) && Objects.equals(date, message1.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(from, to, message, date);
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "from=" + from +
+                ", to=" + to +
+                ", message='" + message + '\'' +
+                ", date=" + date +
+                '}';
+    }
 }
