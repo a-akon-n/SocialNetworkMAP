d63b4c2046f97d2fbf58362620c679c87e7a5130
Ali Akan <ali.akan.narcis01@gmail.com>
2021-12-02 13:13:54 +0200

    small edits in main

--- src/ro/ubbcluj/map/Main.java
+++ src/ro/ubbcluj/map/Main.java
@@ -1,10 +1,9 @@
 package ro.ubbcluj.map;
 
-import ro.ubbcluj.map.domain.Friendship;
-import ro.ubbcluj.map.domain.User;
 import ro.ubbcluj.map.domain.validators.FriendshipValidator;
 import ro.ubbcluj.map.domain.validators.UserValidator;
-import ro.ubbcluj.map.repository.FileRepository;
+import ro.ubbcluj.map.repository.inSQL.SQLFriendshipRepository;
+import ro.ubbcluj.map.repository.inSQL.SQLUserRepository;
 import ro.ubbcluj.map.service.FriendshipService;
 import ro.ubbcluj.map.service.Network;
 import ro.ubbcluj.map.service.UserService;
@@ -13,17 +12,22 @@ import ro.ubbcluj.map.ui.Console;
 public class Main {
 
     public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/social_network";
+        String username = "postgres";
+        String password = "12345";
+
         UserValidator userValidator = new UserValidator();
-        FileRepository<Long, User> userRepository = new FileRepository<Long, User>(userValidator, "data/user.csv");
+        SQLUserRepository userRepository = new SQLUserRepository(userValidator, url, username, password);
         UserService userService = new UserService(userRepository);
 
-        FriendshipValidator friendshipValidator = new FriendshipValidator();
-        FileRepository<Long, Friendship> friendshipRepository = new FileRepository<Long, Friendship>(friendshipValidator, "data/friendship.csv");
-        FriendshipService friendshipService = new FriendshipService(friendshipRepository, userRepository);
+        FriendshipValidator friendshipValidator = new FriendshipValidator(userRepository);
+        SQLFriendshipRepository friendshipRepository = new SQLFriendshipRepository(friendshipValidator, url, username, password);
+        FriendshipService friendshipService = new FriendshipService(friendshipRepository);
 
         Network network = new Network(userRepository, friendshipRepository);
 
         Console userInterface = new Console(userService, friendshipService, network);
+
         userInterface.run_console();
     }
 }
