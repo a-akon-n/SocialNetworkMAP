72bb93c00c7737605521d2d149bb65a7315709d9
Ali Akan <ali.akan.narcis01@gmail.com>
2021-12-02 13:11:27 +0200

    new service uses sql repository

--- src/ro/ubbcluj/map/service/FriendshipService.java
+++ src/ro/ubbcluj/map/service/FriendshipService.java
@@ -2,51 +2,22 @@ package ro.ubbcluj.map.service;
 
 import ro.ubbcluj.map.domain.Friendship;
 import ro.ubbcluj.map.domain.User;
-import ro.ubbcluj.map.repository.FileRepository;
+import ro.ubbcluj.map.repository.inSQL.SQLFriendshipRepository;
 
-import java.util.ArrayList;
+import java.util.Objects;
 
 public class FriendshipService extends AbstractService<Long, Friendship> {
-
-    private FileRepository<Long, User> userRepo;
-    public FriendshipService(FileRepository<Long, Friendship> repository, FileRepository<Long, User> userRepo) {
-        super(repository);
-        this.userRepo = userRepo;
+    public FriendshipService(SQLFriendshipRepository friendshipRepository) {
+        repository = friendshipRepository;
     }
 
     @Override
     public void addEntity(Friendship entity) {
+        for(Friendship friendship:repository.findAll()){
+            if(Objects.equals(friendship.getUser1(), entity.getUser1()) && Objects.equals(friendship.getUser2(), entity.getUser2())){
+                throw new IllegalArgumentException("Entitatea deja exista!");
+            }
+        }
         super.addEntity(entity);
-        entity.getUser1().addFriend(entity.getUser2());
-        entity.getUser2().addFriend(entity.getUser1());
-    }
-
-    @Override
-    public void deleteEntity(Long aLong) {
-        Friendship friendship = findOne(aLong);
-        friendship.getUser1().deleteFriend(friendship.getUser2());
-        friendship.getUser2().deleteFriend(friendship.getUser1());
-        super.deleteEntity(aLong);
-    }
-
-    @Override
-    public void writeData(ArrayList<Friendship> entities) {
-        StringBuilder data = new StringBuilder("");
-        for(Friendship friendship:entities){
-            data.append(friendship.getId()).append(";").append(friendship.getUser1().getId()).append(";").append(friendship.getUser2().getId()).append("\n");
-        }
-        repository.writeData(data.toString());
-    }
-
-    @Override
-    public void loadData() {
-        ArrayList<String> data = repository.loadData();
-        for(int i = 0; i < data.size(); i+=3){
-            Long id_friendship = Long.valueOf(data.get(i));
-            User user1 = userRepo.findOne(Long.valueOf(data.get(i+1)));
-            User user2 = userRepo.findOne(Long.valueOf(data.get(i+2)));
-            Friendship friendship = new Friendship(id_friendship, user1, user2);
-            repository.save(friendship);
-        }
     }
 }
--- src/ro/ubbcluj/map/service/UserService.java
+++ src/ro/ubbcluj/map/service/UserService.java
@@ -1,35 +1,22 @@
 package ro.ubbcluj.map.service;
 
 import ro.ubbcluj.map.domain.User;
-import ro.ubbcluj.map.repository.FileRepository;
+import ro.ubbcluj.map.repository.inSQL.SQLUserRepository;
 
-import java.util.ArrayList;
+import java.util.Objects;
 
-public class UserService extends AbstractService<Long, User> {
-
-    public UserService(FileRepository<Long, User> repository) {
-        super(repository);
+public class UserService extends AbstractService<Long, User>{
+    public UserService(SQLUserRepository userRepository) {
+        repository = userRepository;
     }
 
     @Override
-    public void writeData(ArrayList<User> entities) {
-        StringBuilder data = new StringBuilder();
-        for(User user:entities){
-            data.append(user.getId()).append(";").append(user.getFirstName()).append(";").append(user.getLastName()).append("\n");
+    public void addEntity(User entity) {
+        for(User user:repository.findAll()){
+            if(Objects.equals(user.getFirstName(), entity.getFirstName()) && Objects.equals(user.getLastName(), entity.getLastName())){
+                throw new IllegalArgumentException("Entitatea deja exista!");
+            }
         }
-        repository.writeData(data.toString());
+        super.addEntity(entity);
     }
-
-    @Override
-    public void loadData() {
-        ArrayList<String> data = repository.loadData();
-        for(int i = 0; i < data.size(); i+=3){
-            Long id_user = Long.valueOf(data.get(i));
-            String first_name = data.get(i+1);
-            String last_name = data.get(i+2);
-            User user = new User(id_user, first_name, last_name);
-            repository.save(user);
-        }
-    }
-
 }
